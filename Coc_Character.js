db401b5574aa19bba54e431415b593bbe51bc82d
ychleo102615 <leo102615@gmail.com>
2018-02-22 00:44:31 +0800

    make radom function on the top

--- Coc_Character.js
+++ Coc_Character.js
@@ -1,5 +1,10 @@
 const MAX_SUCESS_RATE = 99;
 const AUX_VALUE = 20;
+
+getRamdomSucessRateValue = () => {
+    return Math.floor((Math.random() * MAX_SUCESS_RATE));
+};
+
 class Participants{
     constructor(name){
         this.name = name;
@@ -16,22 +21,23 @@ class Participants{
             listenSucessRate: getRamdomSucessRateValue()
         };
         this.health = {
-            maxHealth = 10,
-            currentHealth = 10
+            maxHealth: 10,
+            currentHealth: 10
         };
     }
 
-    getRamdomSucessRateValue = () => {
-        return Math.floor((Math.random() * MAX_SUCESS_RATE) + 1);
+    showSatus (){
+        console.log(`${this.name}'s status:\n
+                    attack: ${this.equippedWeapon.attackSucessRate}`);
     }
 
-    takeInjuries = (value) => {
-        currentHealth -= value;
-        if(currentHealth < 0)
-            this.reportDeath;
+    takeInjuries (value) {
+        this.health.currentHealth -= value;
+        if(this.health.currentHealth < 0)
+        this.reportDeath();
     }
 
-    reportDeath = () => {
+    reportDeath () {
         console.log(`${this.name} is dead.`);
     }
 }
@@ -51,3 +57,20 @@ class Thinker extends Participants{
         this.skill.listenSucessRate += AUX_VALUE;
     }
 }
+
+test = () =>{
+    p1 = new Acter(`p1`);
+    p2 = new Thinker(`p2`);
+
+    p1.showSatus();
+    p2.showSatus();
+
+    if(p1.equippedWeapon.attackSucessRate > 50)
+        p2.takeInjuries(3);
+    else{
+        p1.takeInjuries(3);
+        console.log(`p1 get injuried.`);
+    }
+}
+
+test();
